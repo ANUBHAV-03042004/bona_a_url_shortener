792e0100252e92d41486c620e6b20a52e4d66085
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-04-10 19:13:36 +0530

    worked on url_shortener using jade

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ mongoose.connect(mongo_url, {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'views')));
 
